630f0dade8c0d468c1327919af61e38fd19804c3
vitaliykreminskiy <vkrmk13@gmail.com>
2023-03-11 23:30:18 +0200

    added aggregation for extracting recipes count in cuisine

--- src/routers/cuisines.ts
+++ src/routers/cuisines.ts
@@ -12,7 +12,25 @@ const COLLECTION = 'cuisines'
 CuisinesRouter.get('/', async (req, res) => {
   const collection = Database.db.collection(COLLECTION)
 
-  const cuisines = await collection.find().toArray()
+  const cuisines = await collection
+    .aggregate([
+      {
+        $lookup: {
+          from: 'recipes',
+          localField: '_id',
+          foreignField: 'cuisineId',
+          as: 'recipes',
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          count: { $size: '$recipes' },
+        },
+      },
+    ])
+    .toArray()
 
   res.json(cuisines)
 })
--- src/routers/recipes.ts
+++ src/routers/recipes.ts
@@ -1,3 +1,53 @@
 import Express, { Request } from 'express'
+import Database from '../classes/Database'
+
+enum Units {
+  tsp = 'tsp',
+  tbsp = 'tbsp',
+  pieces = 'pieces',
+  grams = 'grams',
+  miligrams = 'miligrams',
+  kilograms = 'kilograms',
+}
+
+type IngredientAmount = {
+  amount: number
+  unit: keyof Units
+}
+
+type Ingredient = {
+  name: string
+  amount: IngredientAmount
+}
+
+type Recipe = {
+  title: string
+  cookingTime: string
+  cookingsteps: string[]
+  ingredients: Ingredient[]
+
+  cuisineId: string
+}
+
+const COLLECTION = 'recipes'
 
 export const RecipesRouter = Express.Router()
+
+RecipesRouter.post('/', (req: Request<{}, {}, Recipe>, res) => {
+  Database.db
+    .collection(COLLECTION)
+    .insertOne(req.body)
+    .then((result) => {
+      console.log(result)
+
+      return res.send()
+    })
+})
+
+RecipesRouter.get('/', async (req, res) => {
+  const collection = Database.db.collection(COLLECTION)
+
+  const cuisines = await collection.find().toArray()
+
+  res.json(cuisines)
+})
